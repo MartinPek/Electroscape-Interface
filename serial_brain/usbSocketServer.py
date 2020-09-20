b21e13ec5fe9dc00b221ff7dd1c1d8bc0ad668c9
asdasd <sny.x@web.de>
2020-09-20 13:01:58 +0200

    converted socketserver to a class, now accept input other than usb

--- serial_brain/usbSocketServer.py
+++ serial_brain/usbSocketServer.py
@@ -30,97 +30,85 @@ global sock
 clients = []
 
 
-def connect_serial():
-    while True:
-        ports = glob.glob('/dev/ttyUSB[0-9]')
-        for usb_port in ports:
+class SocketServer:
+    def __init__(self, port):
+        self.clients = []
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # this fixes socket.error: [Errno 98] Address already in use
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        # empty IP means its will accept from any connection, we could predefine some later
+        # for now its only used on the Pi and GM potentially in the future
+        self.sock.bind(("127.0.0.1", socket_port))
+        # maximum of 5 connection allowed
+        self.sock.listen(5)
+        # sock.settimeout(5)
+        thread = Thread(target=self.__manage_sockets)
+        thread.start()
+        self.__main()
+
+    def __connect_serial(self):
+        while True:
+            ports = glob.glob('/dev/ttyUSB[0-9]')
+            for usb_port in ports:
+                try:
+                    ser = serial.Serial(usb_port, baud)
+                    print("serial found!")
+                    return ser
+                except OSError as err:
+                    if err.errno == 13:
+                        print("Permission error")
+                    print(err)
+            print("no serial found, checking again")
+            sleep(0.5)
+
+    def __manage_sockets(self):
+        print('starting to seek connection on the socket')
+        while True:
+            client, address = self.sock.accept()
+            self.clients.append(client)
+            print('Got connection from', address)
+            client.send(b'hello there, you will be listening to the Arduino\n')
+
+    def __transmit(self, line):
+        line = line.encode()
+        for client in self.clients:
             try:
-                ser = serial.Serial(usb_port, baud)
-                print("serial found!")
-                return ser
-            except OSError as err:
-                if err.errno == 13:
-                    print("Permission error")
-                print(err)
-        print("no serial found, checking again")
-        sleep(0.5)
+                client.send(line)
+            except socket.error as msg:
+                print("Socket transmission Error: {}".format(msg))
+                print("a client dropped")
+                self.clients.remove(client)
 
+    def __handle_serial(self, ser):
+        print("starting to monitor")
+        while ser is not None and ser.is_open:
+            line = self.__read_serial(ser)
+            if not line and type(line) is bool:
+                print("serial connection lost")
+                return
+            if line:
+                self.__transmit(line)
 
-def setup_socket():
-    global sock
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    # this fixes socket.error: [Errno 98] Address already in use
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    print("Socket successfully created")
-
-    # empty IP means its will accept from any connection, we could predefine some later
-    # for now its only used on the Pi and GM potentially in the future
-    sock.bind(("127.0.0.1", socket_port))
-
-    # maximum of 5 connection allowed
-    sock.listen(5)
-    # sock.settimeout(5)
-
-
-def transmit(line):
-    line = line.encode()
-    for client in clients:
+    def __read_serial(self, ser):
         try:
-            client.send(line)
-        except socket.error as msg:
-            print("Socket transmission Error: {}".format(msg))
-            print("a client dropped")
-            clients.remove(client)
+            line = str(ser.readline())[2:][:-5]
+            return line
+        except ValueError as e:
+            print("ValueError")
+            print(e)
+            return False
+        except Exception as e:
+            print("unexpected exception")
+            print(e)
+            ser.close()
+            return False
 
-
-def read_serial(ser):
-    try:
-        line = str(ser.readline())[2:][:-5]
-        return line
-    except ValueError as e:
-        print("ValueError")
-        print(e)
-        return False
-    except Exception as e:
-        print("unexpected exception")
-        print(e)
-        ser.close()
-        return False
-
-
-def handle_serial(ser):
-    print("starting to monitor")
-    while ser is not None and ser.is_open:
-        line = read_serial(ser)
-        if not line and type(line) is bool:
-            print("serial connection lost")
-            return
-        if line:
-            transmit(line)
-
-
-# for now limited to a single client, would like to expand this in the future but for now
-# this has to do
-def manage_sockets():
-    print('starting to seek connection on the socket')
-    global clients
-    while True:
-        client, address = sock.accept()
-        clients.append(client)
-        print('Got connection from', address)
-        client.send(b'hello there, you will be listening to the Arduino\n')
-
-
-def main():
-    setup_socket()
-    thread = Thread(target=manage_sockets)
-    thread.start()
-    # this loops back when the serial connection breaks, so in case of a disconnect it reconnects
-    while True:
-        ser = connect_serial()
-        handle_serial(ser)
+    def __main(self):
+        # this loops back when the serial connection breaks, so in case of a disconnect it reconnects
+        while True:
+            ser = self.__connect_serial()
+            self.__handle_serial(ser)
 
 
 if __name__ == '__main__':
-    main()
+    SocketServer(socket_port)
